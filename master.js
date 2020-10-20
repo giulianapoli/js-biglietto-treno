c97d8585f8bdb39baa3aaec41f2dfc0d82c460f6
Giulia Napoli <gnapoli1996@gmail.com>
2020-10-20 18:07:46 +0200

    Adds to.Fixed function

--- master.js
+++ master.js
@@ -1,14 +1,16 @@
 var km = 0.21
-var kmMinorenni = 0.21*0.8
-var kmOver65 = 0.21*0.6
+var kmMinorenni = 0.21 - (0.21*0.8)
+var kmOver65 = 0.21 - (0.21*0.6)
+
 
 var anni = prompt("Quanti anni hai?")
 var numeroKm = prompt("quanti kilometri devi percorrere?")
 
 if (anni < 18) {
-  document.getElementById("ride").innerHTML = "Il costo della tua corsa è di " + kmMinorenni*numeroKm;
+  document.getElementById("ride").innerHTML = "Il costo della tua corsa è di " +
+   (kmMinorenni*numeroKm).toFixed(2);
 } else if (anni > 65) {
-    document.getElementById("ride").innerHTML = "Il costo della tua corsa è di " + kmOver65*numeroKm;
+    document.getElementById("ride").innerHTML = "Il costo della tua corsa è di " + (kmOver65*numeroKm).toFixed(2);
 } else {
-  document.getElementById("ride").innerHTML = "Il costo della tua corsa è di " + km*numeroKm;
+  document.getElementById("ride").innerHTML = "Il costo della tua corsa è di " + (km*numeroKm).toFixed(2);
 }
